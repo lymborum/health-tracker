d5b988a5f7abc21b2738a91c6cbc23da1f895baf
Vetner <73942639+lymborum@users.noreply.github.com>
2026-09-06 13:24:07 +0800

    sw network-first

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE="tasks-v1";
+const CACHE="tasks-v2";
 self.addEventListener("install",e=>{self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(clients.claim())});
 self.addEventListener("fetch",e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+ e.respondWith(fetch(e.request).then(res=>{
   const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;
- }).catch(()=>caches.match(e.request))));
+ }).catch(()=>caches.match(e.request)));
 });
